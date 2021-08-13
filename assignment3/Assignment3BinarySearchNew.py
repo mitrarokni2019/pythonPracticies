ab29cf19523023a19db22017d9b9a537a4fb7592
mitra rokni <mitra.rokni.0545@student.uu.se>
2021-08-13 13:12:30 +0200

    binary tree first edition

--- assignment3/Assignment3BinarySearchNew.py
+++ assignment3/Assignment3BinarySearchNew.py
@@ -7,6 +7,9 @@ Collection of key-value pair records, implemented as a binary search tree.
 
 
 
+from Assignment3LinkedList import Node
+
+
 class Dictionary:
     class Node:
         def __init__(self, key, value):
@@ -44,16 +47,21 @@ class Dictionary:
         def __str__(self):
             return str(self.key) + ":" + str(self.value)   # Abstract method, add your own
         
-        def __delete(self, key):      # Help method for delete (optional)
-            pass   # Abstract method, add your own
-        
-        def __getSmallest(self):   
-            """   p = self
-            while p.left is not None:
-                p = p.left
-                return p   # Method used by __delete (optional)
-            # Abstract method, add your own """
-            pass
+        # def __delete(self, key):
+            
+        #     if(self.key==None):
+        #         print("Node is empty")
+        #     if(self.key!=key):
+        #         print("Key is invalid")                
+        #     else:
+        #         return self == None  # Help method for delete (optional)
+               
+        def __getSmallest(self):
+            current_node = self
+                
+            while current_node.left_child:
+                current_node = current_node.left_child
+            return current_node
         
 # Dictionary methods start here:
     def __init__(self):
@@ -87,32 +95,6 @@ class Dictionary:
             return True
         else:
             self._insert(self.root, key, value)
-        
-        
-
-
-
-
-
-
-        """ temp=self.root
-        if self.root == None:
-            self.root= Node(key,value)
-            return True
-
-        else: 
-            if  value < self.root.value:
-                if self.root.right is None:
-                    self.root.right = Node(key, value)
-                else:
-                    return insert(temp, )
-            
-            if value > self.__tree.value:
-                if self.__tree.left is None:
-                    self.__tree.nextNode """
-
-                         
-            # Abstract method, add your own
 
     """
     Returns value that is identified by `key`, or None if no such key exists.
@@ -141,9 +123,7 @@ class Dictionary:
             
             else:
                 return self._get(temp.right ,key)
-
-
-          
+    
     def get(self, key):
         if self.root == None:
             print(" Dic is empty there is no value to return ")
@@ -155,11 +135,37 @@ class Dictionary:
     """
     Delete key-value pair identified by `key` and returns 'True' if deleted, 'False' if not found in the Dictionary.
     """
-    def delete(self, key):
+    def _delete(self, temp,  key):
+        
+        if key < temp.key:
+            temp =  _delete(temp.left, key)
+            
 
+        elif key > temp.key:
+            temp = _delete(temp.right, key)
+            
 
+        elif key == temp.key:
+            if temp.right is None:
+                temp =  temp.left
+                return temp
 
-        pass   # Abstract method, add your own
+            elif temp.left is None:
+                temp = temp.right
+                return temp
+
+            else: 
+                temp= __getSmallest(self)           
+                 
+        else:
+            print("there is no such akey in dic!")     # Abstract method, add your own
+
+        def delete(self, key):
+            if self.root == None:
+             print(" Dic is empty there is no value to be deleted ")
+            else:
+                return self._delete(self.root, key)
+            
     
     """
         Returns a gererator that could iterate over the tupel (key, value) objects (orderd by key, smallest to largest)
@@ -189,6 +195,8 @@ ww.insert("k5", "5")
 ww.insert("k7", "7")
 ww.get("k3")
 print(ww.get("k3"))
+print(ww.get("k7"))
+
 
 
 
